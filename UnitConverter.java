604d8aef7883589dc06315c5b281908892e273d6
Luke Keating <keatingl@uci.edu>
2021-06-23 09:33:43 -0700

    Fixed US Gallons to Imperial Gallons. Added error handling.

--- UnitConverter.java
+++ UnitConverter.java
@@ -3,30 +3,33 @@ import java.util.Scanner;
 public class UnitConverter {
 	public static void main(String args[]) {
 		Scanner scanner = new Scanner(System.in);
-		boolean running = true;
-		while (running) {
+		while (true) {
 			//do nothing
-			System.out.println("Please select an option:\n"
-					+ "1. Cups to Teaspoons\n"
-					+ "2. Miles to Kilometers\n"
-					+ "3. US Gallons to Imperial Gallons\n"
-					+ "4. Quit");
-			int option = Integer.valueOf(scanner.nextLine());
-			System.out.println("Enter a value.");
-			int value = Integer.valueOf(scanner.nextLine());
-			switch (option) {
-			case 1:
-				System.out.println(value + " Cups is " + value * 48 + " Teaspoons");
-				break;
-			case 2:
-				System.out.println(value + " Miles is " + value * 1.609 + " Kilometers");
-				break;
-			case 3:
-				System.out.println(value + " US Gallons is " + value * 48 + " Imperial Gallons");
-				break;
-			case 4:
-				running = false;
-				break;
+			try {
+				System.out.println("Please select an option:\n"
+						+ "1. Cups to Teaspoons\n"
+						+ "2. Miles to Kilometers\n"
+						+ "3. US Gallons to Imperial Gallons\n"
+						+ "4. Quit");
+				int option = Integer.valueOf(scanner.nextLine());
+				if (option==4) {
+					break;
+				}
+				System.out.println("Enter a value.");
+				float value = Float.parseFloat(scanner.nextLine());
+				switch (option) {
+				case 1:
+					System.out.println(value + " Cups is " + value * 48 + " Teaspoons");
+					break;
+				case 2:
+					System.out.println(value + " Miles is " + value * 1.609 + " Kilometers");
+					break;
+				case 3:
+					System.out.println(value + " US Gallons is " + value/1.201 + " Imperial Gallons");
+					break;
+				}
+			} catch(Exception e) {
+				System.out.println("You got " + e + ". Please try again.");
 			}
 		}
 	}
